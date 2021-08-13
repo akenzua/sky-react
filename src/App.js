808efbe83bd3895f3e3fab7faaded71da34f9c57
Akintade Ajibade <ajibade.akintade@gmail.com>
2021-08-13 08:36:24 +0100

    added App component with Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <main>Search</main>;
-}
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/Search";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" component={Search} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
